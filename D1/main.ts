13d65e90640518490a0c6bcdc46184971ab00e10
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2024-12-02 15:30:24 +0100

    Sort table

--- D1/main.ts
+++ D1/main.ts
@@ -3,5 +3,6 @@ import fs from "node:fs";
 const inputText = fs.readFileSync("./input.txt", "utf-8");
 const inputLines = inputText.split("\n");
 
-const left = inputLines.map(line => line.split(/\s+/)[0]).filter(Boolean);
-const right = inputLines.map(line => line.split(/\s+/)[1]).filter(Boolean);
+const left = inputLines.map(line => line.split(/\s+/)[0]).filter(Boolean).sort((a, b) => Number(a) - Number(b));
+const right = inputLines.map(line => line.split(/\s+/)[1]).filter(Boolean).sort((a, b) => Number(a) - Number(b));
+
