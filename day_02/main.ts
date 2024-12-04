b5dcd0052151bdee38be37cae870361c7e8e337a
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2024-12-04 17:16:57 +0100

    [Feat]: Add line checkers

--- day_02/main.ts
+++ day_02/main.ts
@@ -4,13 +4,76 @@ import { INodeSystemError } from "./interface";
 async function main(fileUrl: string) {
     try {
         const inputFile = fs.readFileSync(fileUrl, "utf-8");
-        console.log(inputFile);
+        const inputLines = inputFile.split('\n');
+        let safeLine = 0
+
+        for (let lineIndex = 0; lineIndex < inputLines.length; lineIndex++) {
+            const element: string = inputLines[lineIndex].replaceAll(" ", "");
+            const firstDigit = Number(element[0])
+            const secondDigit = Number(element[1])
+            let isUnsafe = 0
+
+
+            if (isSafeEarlyCheck(firstDigit, secondDigit)) {
+                const isAscendingLine: boolean = isAscending(firstDigit, secondDigit)
+    
+                for (let stringIndex = 0; stringIndex < element.length - 1; stringIndex++) {
+                    const leftDigit = Number(element[stringIndex]);
+                    const rightDigit = Number(element[stringIndex + 1]);
+                    console.log("left: ",leftDigit," right: ", rightDigit)
+                    const isAscendingDigit = isAscending(leftDigit, rightDigit);
+    
+                    if ((isAscendingDigit && !isAscendingLine) || (!isAscendingDigit && isAscendingLine)) {
+                        console.log("Found unsafe pattern - incrementing isUnsafe");
+                        isUnsafe++;
+                    }
+                }
+                if (isUnsafe === 0) {
+                    console.log(`Line{${element}} is safe - incrementing safeLine`);
+                    safeLine++;
+                } else {
+                    console.log(`Line{${element}} is unSafe`)
+                }
+            }
+
+        }
+
+        console.log(`There is : ${safeLine} number of safe line`)
     } catch (error) {
         if (error instanceof Error) {
-            const nodeError: INodeSystemError = error as INodeSystemError;
+            checkError(error);
+        }
+    }
+}
+
+function isAscending(firstDigit: number, secondDigit: number): boolean {
+    if (firstDigit - secondDigit < 0) return true
+    return false
+}
+
+function isSafeEarlyCheck(firstDigit: number, secondDigit: number): boolean {
+    if (Number.isNaN(firstDigit) || Number.isNaN(secondDigit)) {
+        console.log("NaN check failed");
+        return false;
+    }
+    if (firstDigit - secondDigit === 0) {
+        console.log(firstDigit, secondDigit)
+        return false;
+    }
+    console.log("All early checks passed");
+    return true;
+}
+
+function checkLine(line: string, isAscending: boolean) {
+
+}
+
+
+function checkError(error: unknown) {
+    const nodeError: INodeSystemError = error as INodeSystemError;
             switch (nodeError.code) {
                 case "ENOENT":
-                    console.error(`[Error: ${nodeError.code}] File not found using: ${fileUrl}`);
+                    console.error(`[Error: ${nodeError.code}] File not found`);
                     break;
             
                 default:
@@ -18,8 +81,6 @@ async function main(fileUrl: string) {
                     console.error(nodeError);
                     break;
             }
-        }
-    }
 }
 
 main(process.argv[2])
